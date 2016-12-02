eb4b9bd68681845edccaee2e7af19e0f0699bdfb
Pedro Alves <pedro_alves@me.com>
2016-12-02 16:33:02 -0200

    adding seed to create user and password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+if User.all.empty?
+  user = User.where(email: 'test@marvel.com')first_or_create( email:                'test@marvel.com',
+                                                              password:             '123456',
+                                                              password_confirmation:'123456'
+                                                            )
+
+  puts "=============================== User -> 'test@marvel.com' <- created successfully and his password is -> '123456' <- ==============================="
+end 
