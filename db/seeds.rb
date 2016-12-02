eb78d02edda27fb2915f70ca3aef4aa34ffbfe62
Pedro Alves <pedro_alves@me.com>
2016-12-02 17:06:41 -0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 if User.all.empty?
-  user = User.where(email: 'test@marvel.com')first_or_create( email:                'test@marvel.com',
+  user = User.where(email: 'test@marvel.com').first_or_create( email:                'test@marvel.com',
                                                               password:             '123456',
                                                               password_confirmation:'123456'
                                                             )
